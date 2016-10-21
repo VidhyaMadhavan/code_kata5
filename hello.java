faf659f56ab5ed9d9569438d195b36a9d4b3cc89
VidhyaMadhavan <vidhya.m1818@gmail.com>
2016-10-21 11:39:50 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,11 +1,16 @@
 import java.io.*;
+import java.util.*;
 class hello
 {
 public static void main(String args[])
 {
+  Scanner sc=new Scanner(System.in);
+  String str;
+  System.out.println("Enter string:");
+  str=sc.nextLine();
 for(int i=0;i<5;i++)
 {
-System.out.println("Hello");
+System.out.println(str);
 }
 }
 }
